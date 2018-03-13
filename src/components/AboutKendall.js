a91c6c58ec1d378ca09ac5d0e1dfc0fc6d5e450a
Devin Gray <devin.gray@colorado.edu>
2018-03-13 00:36:15 -0600

    about section better

--- src/components/AboutKendall.js
+++ src/components/AboutKendall.js
@@ -33,12 +33,12 @@ export default class Home extends Component {
 
 
 
-        <div className="info">
+
 
 
         <div className='about-kendall'>
 
-Kendall has been a nester since her very first space to call her own - her college dorm room.
+<p className="work"> Kendall has been a nester since her very first space to call her own - her college dorm room.
 
 Inheriting the “decorating gene” from her mother who was a single mom on a shoestring budget,
 
@@ -46,22 +46,40 @@ Kendall learned to see the beauty in flea market finds, tag sale items, and alle
 
 she evolved into adulthood the mix of inherited items, travel treasures, splurge with budget pieces
 
-have been a part of her decorating passion.
+have been a part of her decorating passion. </p>
 
-As a “stylist” compared to an interior decorator she tends to skip the design professionals-only showrooms
+<p className="work"> As a “stylist” compared to an interior decorator she tends to skip the design professionals-only showrooms
 
 and does a lot of the hunting in your home, tag sales, flea markets, etsy, etc. With all that in hand she
 
 loves to take a room and refine it through details of making it your own.  This decor style allows your personality to shine through while keeping in mind everyday functionality.
 
-Your home is your sanctuary and should reflect who you are from the very moment someone walks through your door.
+Your home is your sanctuary and should reflect who you are from the very moment someone walks through your door.</p>
 
-Before moving to Colorado in 1996 Kendall studied Fine Arts at the University of Kentucky and received her Bachelor of Fine Arts.
+
+
+<p className="work">Before moving to Colorado in 1996, Kendall studied Fine Arts at the University of Kentucky and received her Bachelor of Fine Arts.
 
 She loves being part of the Stapleton community.  A wife of 20 years, and a mother of two boys -12 and 14.
-</div>
+</p>
 
-        </div>
+      </div>
+
+
+
+      <div className="coco">
+        <p className="quotes"> "An interior is the natural projection of our soul.”  - Coco Chanel </p>
+      </div>
+      <div className="berkus">
+        <p className="quotes"> “Your home should tell the story of who you are, and be a collection of what you love.”
+          -Nate Berkus </p>
+      </div>
+      <div className="flett">
+        <p className="quotes"> “Collect things you love, that are authentic to you, and your house becomes a story.”  - Erin Flett </p>
+      </div>
+
+
+
 
 
 
